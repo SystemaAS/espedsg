4e5e775df506f105eeaf6c80bd594fb0d06d34b7
Oscar de la Torre <oscar@systema.no>
2016-03-18 17:40:50 +0100

    update

---
+++ src/no/systema/tvinn/sad/sadimport/model/jsonjackson/topic/JsonSadImportSpecificTopicFaktTotalRecord.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package no.systema.tvinn.sad.sadimport.model.jsonjackson.topic;
+
+/**
+ * @author oscardelatorre
+ * @date Mar 20, 2016
+ * 
+ */
+public class JsonSadImportSpecificTopicFaktTotalRecord {
+
+	private String tot_vakd = null;
+	public void setTot_vakd(String value) {  this.tot_vakd = value; }
+	public String getTot_vakd() {return this.tot_vakd;}
+	
+	private String tot_vaku = null;
+	public void setTot_vaku(String value) {  this.tot_vaku = value; }
+	public String getTot_vaku() {return this.tot_vaku;}
+	
+	private String tot_omr = null;
+	public void setTot_omr(String value) {  this.tot_omr = value; }
+	public String getTot_omr() {return this.tot_omr;}
+	
+	private String tot_fabl = null;
+	public void setTot_fabl(String value) {  this.tot_fabl = value; }
+	public String getTot_fabl() {return this.tot_fabl;}
+	
+	
+}
