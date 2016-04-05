026703960e0d9398c589f06be546c1235ceb0003
Oscar de la Torre <oscar@systema.no>
2016-04-05 11:42:03 +0200

    update

---
+++ WebContent/WEB-INF/resources/js/headerSkat.js
@@ -0,0 +1,26 @@
+	//this variable is a global jQuery var instead of using "$" all the time. Very handy
+  	var jq = jQuery.noConflict();
+  	var BLOCKUI_OVERLAY_MESSAGE_DEFAULT = "Please wait...";
+    
+  	jq(document).ready(function() {
+  		//TODO
+  	});
+  	
+  	//Overlay on tab (to mark visually a delay...)
+    jq(function() {
+	  //Overlay - General Header Menus
+  	  jq('#alinkTopicListMenuImp').click(function() { 
+  		  jq.blockUI({ message: BLOCKUI_OVERLAY_MESSAGE_DEFAULT});
+  	  });
+  	  jq('#alinkTopicListMenuExp').click(function() { 
+  		  jq.blockUI({ message: BLOCKUI_OVERLAY_MESSAGE_DEFAULT});
+  	  });
+  	  jq('#alinkTopicListMenuNctsExp').click(function() { 
+  		  jq.blockUI({ message: BLOCKUI_OVERLAY_MESSAGE_DEFAULT});
+  	  });
+  	  jq('#alinkTopicListMenuNctsImp').click(function() { 
+  		  jq.blockUI({ message: BLOCKUI_OVERLAY_MESSAGE_DEFAULT});
+  	  });
+    });
+  	
+  	
