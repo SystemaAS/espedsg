40719320a97b3988094971359756663b255777a7
Oscar de la Torre <oscar@systema.no>
2016-04-28 13:05:37 +0200

    update

---
+++ src/no/systema/skat/nctsexport/model/jsonjackson/topic/JsonSkatNctsExportTopicCopiedFromTransportUppdragContainer.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package no.systema.skat.nctsexport.model.jsonjackson.topic;
+
+import java.util.Collection;
+
+
+
+/**
+ * @author oscardelatorre
+ * @date Apr 28, 2014
+ * 
+ *
+ */
+public class JsonSkatNctsExportTopicCopiedFromTransportUppdragContainer {
+	private String user = null;
+	public void setUser(String value) {  this.user = value; }
+	public String getUser() { return this.user;}
+	
+	private String sign = null;
+	public void setSign(String value) {  this.sign = value; }
+	public String getSign() { return this.sign;}
+	
+	private String avd = null;
+	public void setAvd(String value) {  this.avd = value; }
+	public String getAvd() { return this.avd;}
+	
+	private String opd = null;
+	public void setOpd(String value) {  this.opd = value; }
+	public String getOpd() { return this.opd;}
+	
+	private String dkih_tuid = null;
+	public void setDkih_tuid(String value) {  this.dkih_tuid = value; }
+	public String getDkih_tuid() { return this.dkih_tuid;}
+	
+	private String errMsg = null;
+	public void setErrMsg(String value) {  this.errMsg = value; }
+	public String getErrMsg() { return this.errMsg;}
+	
+	//Todo more if needed
+	
+}
